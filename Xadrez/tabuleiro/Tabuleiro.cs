c5eea364670d338b55f47e63308d9cb6a8054b2e
Douglas Nunes <paulodnunes2016@gmail.com>
2026-03-29 11:36:43 -0300

    Criando uma excesão no tabuleiro personalizada no tabuleiro

--- Xadrez/tabuleiro/Tabuleiro.cs
+++ Xadrez/tabuleiro/Tabuleiro.cs
@@ -1,4 +1,6 @@
-﻿namespace tabuleiro
+﻿using Xadrez.tabuleiro;
+
+namespace tabuleiro
 {
     internal class Tabuleiro
     {
@@ -16,10 +18,38 @@
         {
             return pecas[linhas,colunas];
         }
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.Coluna,pos.Linha];
+        }
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
         public void colocarPeca(Peca p,Posicao pos)
         {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
             pecas[pos.Linha,pos.Coluna] = p;
             p.posicao = pos;
+        }
+        public bool posicaoValida(Posicao pos)
+        {
+            if(pos.Linha < 0 || pos.Linha>= linhas || pos.Coluna < 0 || pos.Coluna>= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
         }
     }
 }
