75a2826679557743153ec6efb368dd8a2cab92fc
Douglas Nunes <paulodnunes2016@gmail.com>
2026-03-30 18:24:20 -0300

    Testes iniciais - deu errado - aula 218

---
+++ Xadrez/xadrez/PartidaDeXadrez.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using tabuleiro;
+using xadrez;
+
+namespace Xadrez.xadrez
+{
+    internal class PartidaDeXadrez
+    {
+        private Tabuleiro tab;
+        private int turno;
+        private Cor JogadorAtual;
+
+        public PartidaDeXadrez()
+        {
+            tab = new Tabuleiro(8, 8);
+            turno = 1;
+            JogadorAtual = Cor.Branca;
+            colocarPecas();
+        }
+        public void executarMovimento(Posicao origem, Posicao destino)
+        {
+            Peca p = tab.retirarPeca(origem);
+            p.incrementarQteMovimentos();
+
+            Peca pecaCapiturada = tab.retirarPeca(destino);
+            tab.colocarPeca(p,destino);
+        }
+        private void colocarPecas()
+        {
+            tab.colocarPeca(new Torre(tab,Cor.Preta),new PosicaoXadrez('c',1).toPosicao());
+        }
+    }
+}
