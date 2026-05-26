e2893d5dee0e6a784b8e6e9029a848035a2eac9b
Douglas Nunes <paulodnunes2016@gmail.com>
2026-05-26 17:57:37 -0300

    Fazendo excecoes em movimentos possiveis

--- Xadrez/tabuleiro/Peca.cs
+++ Xadrez/tabuleiro/Peca.cs
@@ -19,6 +19,21 @@
         {
             qtdMovimentos++;
         }
+        public bool existeMovimentosPossivel()
+        {
+            bool[,] mat = movimentosPossiveis();
+            for (int i = 0; i < tab.linhas;i++)
+            {
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    if (mat[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
 
         public abstract bool[,] movimentosPossiveis();
 
